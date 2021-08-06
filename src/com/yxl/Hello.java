3b17b649caee6875ad262dcc4cd43cd46476c8cc
yxlong <yxlong77@126.com>
2021-08-06 20:42:53 +0800

    提交Hello.java类

---
+++ src/com/yxl/Hello.java
@@ -0,0 +1,11 @@
+package com.yxl;
+
+/**
+ * @author YXLONG
+ * @create 2021-08-06 @20:33
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
